9b836498e666342203370a55bfb6ae13b36ace32
bliti <pablo.rivera.programmer@gmail.com>
2013-09-26 08:38:28 -0400

    primer push

---
+++ MorningRoutine/MorningRoutine/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  MorningRoutine
+//
+//  Created by bliti on 9/26/13.
+//  Copyright (c) 2013 bliti. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Good morning, world!");
+        
+    }
+    return 0;
+}
+
