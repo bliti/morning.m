0ed411574227abd1dc5ca700a6e1824d69c65fd0
bliti <pablo.rivera.programmer@gmail.com>
2013-09-27 11:01:05 -0400

    morning

--- MorningRoutine/MorningRoutine/main.m
+++ MorningRoutine/MorningRoutine/main.m
@@ -5,22 +5,28 @@
 //  Created by bliti on 9/26/13.
 //  Copyright (c) 2013 bliti. All rights reserved.
 //
-
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[])
 {
-
+    
     @autoreleasepool {
         
-        NSDate * now = [NSDate date];
+        NSDate *now = [NSDate date];
         NSDateFormatter *outputFormatter = [[NSDateFormatter alloc] init];
         [outputFormatter setDateFormat:@"HH:mm"];
         NSString *newDateString = [outputFormatter stringFromDate:now];
-        NSLog(@"It's %@ AM.", newDateString);
-        NSLog(@"Good morning, world!");
+    
+        if (newDateString.intValue > 9) {
+            NSLog(@"You are late.");
+        }
         
+        else {
+            NSLog(@"You are on time.");
+        }
+        
+        NSLog(@"Its %@ AM.", newDateString);
+        NSLog(@"Its time to do your morning practice.");
     }
-    return 0;
+         return 0;
 }
-
